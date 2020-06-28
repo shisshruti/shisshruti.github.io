def0096a3a7e87d875fe9eaa5e113fd456e2c00f
Shruti Goyal <shisshruti@gmail.com>
2020-06-28 16:54:19 +0530

    First commit to my website :)

---
+++ resume.js
@@ -0,0 +1,51 @@
+// Get the modal
+var modal = document.getElementById("myModal");
+var modal1 = document.getElementById("myModal1");
+var modal2 = document.getElementById("myModal2");
+var modal3 = document.getElementById("myModal3");
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+var btn1 = document.getElementById("myBtn1");
+var btn2 = document.getElementById("myBtn2");
+var btn3 = document.getElementById("myBtn3");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+};
+btn1.onclick = function() {
+  modal1.style.display = "block";
+};
+btn2.onclick = function() {
+modal2.style.display = "block";
+};
+  btn3.onclick = function() {
+  modal3.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+  modal1.style.display = "none";
+  modal2.style.display = "none";
+  modal3.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+  if (event.target == modal1) {
+    modal1.style.display = "none";
+  }
+   if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
+   if (event.target == modal3) {
+    modal3.style.display = "none";
+  }
+};
